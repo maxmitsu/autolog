8d40b7739eeb2e21a714c93f552f53b5a378f3aa
maxmitsu <george.fuertes90@gmail.com>
2026-03-13 15:57:49 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "autolog-pwa-v2";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./autolog.css?v=5",
+  "./autolog.js?v=5",
+  "./manifest.json",
+  "./icon-192-custom.png",
+  "./icon-512-custom.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    fetch(req)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then(res => res || caches.match("./index.html")))
+  );
+});
